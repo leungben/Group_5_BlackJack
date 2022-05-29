75d6ddf463b4609eabc56e6b8b54c57c5ddde48c
bened <bened@LAPTOP-8QCTSJF3.ht.home>
2022-05-29 13:02:36 -0400

    Created Group 5 BlackJack Project

---
+++ src/ca/sheridancollege/project/Game.java
@@ -0,0 +1,59 @@
+/**
+ * SYST 17796 Project Base code.
+ * Students can modify and extend to implement their game.
+ * Add your name as an author and the date!
+ */
+package ca.sheridancollege.project;
+
+import java.util.ArrayList;
+
+/**
+ * The class that models your game. You should create a more specific child of this class and instantiate the methods
+ * given.
+ *
+ * @author dancye
+ * @author Paul Bonenfant
+ * @author Prem Parashar
+ */
+public abstract class Game {
+
+    private final String name;//the title of the game
+    private ArrayList<Player> players;// the players of the game
+
+    public Game(String name) {
+        this.name = name;
+        players = new ArrayList();
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @return the players of this game
+     */
+    public ArrayList<Player> getPlayers() {
+        return players;
+    }
+
+    /**
+     * @param players the players of this game
+     */
+    public void setPlayers(ArrayList<Player> players) {
+        this.players = players;
+    }
+
+    /**
+     * Play the game. This might be one method or many method calls depending on your game.
+     */
+    public abstract void play();
+
+    /**
+     * When the game is over, use this method to declare and display a winning player.
+     */
+    public abstract void declareWinner();
+
+}//end class
