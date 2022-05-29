dbac28a6cb1e5dc7e82825109a8a45ab0a599031
leungben <105314068+leungben@users.noreply.github.com>
2022-05-29 13:46:08 -0400

    Added Benedict to Author
    Added Benedict Leung to Author

--- src/ca/sheridancollege/project/Card.java
+++ src/ca/sheridancollege/project/Card.java
@@ -8,8 +8,8 @@ package ca.sheridancollege.project;
 /**
  * A class to be used as the base Card class for the project. Must be general enough to be instantiated for any Card
  * game. Students wishing to add to the code should remember to add themselves as a modifier.
- * @author Prem Parashar
- * @author dancye
+ * @author Benedict Leung
+ * @author 
  */
 public abstract class Card {
     //default modifier for child classes
