1d008e931139e3a6d12466fd4fa67ade05a5b556
bened <bened@LAPTOP-8QCTSJF3.ht.home>
2022-06-07 15:16:19 -0400

    Created design template with description of game(Black Jack) or 21 and added names and ID to comment

--- src/ca/sheridancollege/project/Card.java
+++ src/ca/sheridancollege/project/Card.java
@@ -8,8 +8,18 @@ package ca.sheridancollege.project;
 /**
  * A class to be used as the base Card class for the project. Must be general enough to be instantiated for any Card
  * game. Students wishing to add to the code should remember to add themselves as a modifier.
+ * 
+ * Game: BlackJack - A game that counts 21, this game is usually played against the dealer. In the game, A is considered either 1 or 11
+ * depending on whichever is most beneficial for him. Face cards are all considered 10. The user starts with 2 cards and can either stay
+ * or hit. If he stays and his two cards are higher than the dealer after the dealer makes his move, he wins otherwise he loses. If he hits,
+ * he will gain an extra random card after which he can continue to choose between hit and stay. When the user finally decides to hit stay 
+ * and it becomes the dealer's turn where if the dealer is lower than the user, he hits until his cards are either above or he busts with above 21.
+ * As a side note, if user obtains 21 he automatically Wins regardless of what the dealer has.
+ * 
  * @author Benedict Leung ID: 991329385
- * @author 
+ * @author Dillon Pennell ID: 991388695
+ * @author Sehnajpreet kaur ID: 991655975 
+ * @author Abhai babu  ID: 991659790 
  */
 public abstract class Card {
     //default modifier for child classes
--- src/ca/sheridancollege/project/GroupOfCards.java
+++ src/ca/sheridancollege/project/GroupOfCards.java
@@ -12,9 +12,19 @@ import java.util.Collections;
  * A concrete class that represents any grouping of cards for a Game. HINT, you might want to subclass this more than
  * once. The group of cards has a maximum size attribute which is flexible for reuse.
  *
+ * * Game: BlackJack - A game that counts 21, this game is usually played against the dealer. In the game, A is considered either 1 or 11
+ * depending on whichever is most beneficial for him. Face cards are all considered 10. The user starts with 2 cards and can either stay
+ * or hit. If he stays and his two cards are higher than the dealer after the dealer makes his move, he wins otherwise he loses. If he hits,
+ * he will gain an extra random card after which he can continue to choose between hit and stay. When the user finally decides to hit stay 
+ * and it becomes the dealer's turn where if the dealer is lower than the user, he hits until his cards are either above or he busts with above 21.
+ * As a side note, if user obtains 21 he automatically Wins regardless of what the dealer has.
+ * 
+ * 
+ * 
  * @author Benedict Leung ID: 991329385
- * @author 
- * @author 
+ * @author Dillon Pennell ID: 991388695
+ * @author Sehnajpreet kaur ID: 991655975 
+ * @author Abhai babu  ID: 991659790 
  */
 public class GroupOfCards {
 
